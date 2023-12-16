09cf0506378fa56c601d43c2ffca0d50e442597a
Gabriel Fagundes <113377700+gabrafo@users.noreply.github.com>
2023-12-16 14:40:12 -0300

    update

--- Operações Básicas/Entrada de dados/Main.java
+++ Operações Básicas/Entrada de dados/Main.java
@@ -1,7 +1,7 @@
 import java.util.Locale;
 import java.util.Scanner;
 
-public class entrada
+public class Main
 {
     public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
