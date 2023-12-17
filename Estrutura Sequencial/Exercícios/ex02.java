4a74315e276cfa625b2ab985d4b3cf75d5bf1493
Gabriel Fagundes <113377700+gabrafo@users.noreply.github.com>
2023-12-17 17:05:07 -0300

    upload

---
+++ Estrutura Sequencial/Exercícios/ex02.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ex01 {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int x, y;
+    x = sc.nextInt();
+    y = sc.nextInt();
+    System.out.println("SOMA = " + (x+y));
+
+    sc.close();
+    }
+}
