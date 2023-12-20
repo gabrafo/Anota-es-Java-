5b2eba867aacde4d72afc60504a0690e41d217dd
Gabriel Fagundes <113377700+gabrafo@users.noreply.github.com>
2023-12-20 15:45:48 -0300

    upload

---
+++ Estrutura Condicional/Exercícios/ex01.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class ex01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Escreva um digito: ");
+        int x = sc.nextInt();
+        if(x>=0){
+            System.out.println("Não negativo");
+        } else {
+            System.out.println("Negativo");
+        }
+
+        sc.close();
+    }
+}
