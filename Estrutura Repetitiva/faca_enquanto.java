91b3ecee58bbfc5ff3aabb487482b9b87831a505
Gabriel Fagundes <113377700+gabrafo@users.noreply.github.com>
2024-01-18 17:06:51 -0300

    upload

---
+++ Estrutura Repetitiva/faca_enquanto.java
@@ -0,0 +1,10 @@
+public class faca_enquanto {
+    public static void main(String[] args) {
+        int contador = 0;
+        do{
+            System.out.println(contador);
+            contador++;
+        } while(contador<10);
+        System.out.println(contador);
+    }
+}
