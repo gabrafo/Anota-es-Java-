087418ed652bfc4c7e66288c1f7d61305a85c2d0
Gabriel Fagundes <113377700+gabrafo@users.noreply.github.com>
2024-01-24 15:52:11 -0300

    upload

---
+++ OOP/src/entities/Triangle.java
@@ -0,0 +1,9 @@
+package entities;
+
+public class Triangle {
+
+    // Declaração dos membros da classe
+    public double a; // Atributo do tipo double, o termo "public" permite que a classe seja acessada de outro arquivo
+    public double b;
+    public double c;
+}
