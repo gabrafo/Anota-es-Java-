3994929d9f8eda7ad8a28d862e0b0dd346c49abc
Gabriel Fagundes <113377700+gabrafo@users.noreply.github.com>
2024-01-29 09:10:24 -0300

    update

--- OOP/src/exemplo1/application/Program.java
+++ OOP/src/exemplo1/application/Program.java
@@ -1,6 +1,7 @@
-package application;
+package exemplo1.application;
 
-import entities.Triangle; // Importação da classe Triangle
+import exemplo1.entities.Triangle; // Importação da classe Triangle
+
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -23,10 +24,8 @@ public class Program {
         y.b = sc.nextDouble();
         y.c = sc.nextDouble();
 
-        double p = (x.a+x.b+x.c)/2.0;
-        double areaX = Math.sqrt(p*(p-x.a)*(p-x.b)*(p-x.c));
-        p = (y.a+y.b+y.c)/2.0;
-        double areaY = Math.sqrt(p*(p-y.a)*(p-y.b)*(p-y.c));
+        double areaX = x.area(); // Chamada da função declarada na classe Triangle
+        double areaY = y.area();
 
         System.out.printf("Triangle X area: %.4f%n", areaX);
         System.out.printf("Triangle Y area: %.4f%n", areaY);
