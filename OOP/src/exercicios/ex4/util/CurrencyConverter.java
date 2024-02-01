af649b86f39e6050b0b2d0f5e56de9eb85651fcf
Gabriel Fagundes <113377700+gabrafo@users.noreply.github.com>
2024-02-01 19:12:13 -0300

    delete

--- OOP/src/exercicios/ex4/util/CurrencyConverter.java
+++
@@ -1,9 +0,0 @@
-package exercicios.ex4.util;
-
-public class CurrencyConverter {
-    public static final double tax = 0.06;
-    public static double real(double dollarPrice, double quantity){
-        double real = quantity*dollarPrice;
-        return real + (real*tax);
-    }
-}
