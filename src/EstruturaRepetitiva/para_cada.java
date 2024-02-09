74a9f3782c03195040fa90be8ea047f6bf2100f6
Gabriel Fagundes <gafagundesmesquita@gmail.com>
2024-02-09 13:48:52 -0300

    upload

---
+++ src/EstruturaRepetitiva/para_cada.java
@@ -0,0 +1,21 @@
+package EstruturaRepetitiva;
+
+public class para_cada {
+    public static void main(String[] args) {
+        String[] vect = new String[] {"Maria", "Bob", "Alex"}; // Instancia três posições
+
+        // FOR CONVENCIONAL
+        /*
+        for(int i = 0; i< vect.length; i++){
+            System.out.println(vect[i]);
+        }
+        */
+
+        // FOR EACH
+        for(String obj : vect){ // String é o tipo, "obj" o apelido (poderia ser qualquer um) e "vect" é a coleção
+            System.out.println(obj);
+        }
+
+    }
+}
+
