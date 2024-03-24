bd1e29925da4356981ea741019416c1c81b5f9a7
Gabriel Fagundes <gafagundesmesquita@gmail.com>
2024-03-24 20:52:29 -0300

    upload

---
+++ src/POO/heranca/exemplo2_abstract/entities/enums/Color.java
@@ -0,0 +1,7 @@
+package POO.heranca.exemplo2_abstract.entities.enums;
+
+public enum Color {
+    BLACK,
+    BLUE,
+    RED;
+}
---
+++ src/POO/heranca/exemplo_abstract/entities/SavingsAccount.java
@@ -0,0 +1,31 @@
+package POO.heranca.exemplo_abstract.entities;
+
+public final class SavingsAccount extends Account { // Uso de final IMPEDE que essa classe seja herdada por outra
+    private Double interestRate; // Taxa de juros
+
+    public SavingsAccount() {
+        super();
+    }
+
+    public SavingsAccount(Integer number, String holder, Double balance, Double interestRate) {
+        super(number, holder, balance);
+        this.interestRate = interestRate;
+    }
+
+    public Double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(Double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    public void updateBalance(){
+        balance += balance*interestRate;
+    }
+
+    @Override // Anotação que serve de aviso ao compilador que há uma sobreposição de método
+    public void withdraw(double amount){ // Sobreposição do método withdraw() da classe Account
+        balance-=amount;
+    }
+}
