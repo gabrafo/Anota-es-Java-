2bcf787b0318a7a5e42efe345312a2b94b46f8c8
gabrafo <gafagundesmesquita@gmail.com>
2024-06-18 19:50:42 -0300

    upload

---
+++ src/ProgramacaoFuncionalLambda/exemplo2/application/Program.java
@@ -0,0 +1,28 @@
+package ProgramacaoFuncionalLambda.exemplo2.application;
+
+import GenericsCollection.exemplo8.entities.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program {
+
+    public static int compareProducts(Product p1, Product p2) {
+        return p1.getPrice().compareTo(p2.getPrice());
+    }
+
+    public static void main(String[] args) {
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("TV", 900.00));
+        list.add(new Product("Notebook", 1200.00));
+        list.add(new Product("Tablet", 450.00));
+
+        // Função sendo passada como argumento para outra função, ou seja, como objeto de primeira ordem
+        // Isso é extremamente comum na programação funcional
+        list.sort(Program::compareProducts);
+
+        list.forEach(System.out::println);
+    }
+}
